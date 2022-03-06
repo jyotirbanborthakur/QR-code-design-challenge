730a7ec915b1299e6a3a570daca74adca0bab677
jyotirban borthakur <54855139+jyotirbanborthakur@users.noreply.github.com>
2022-03-06 13:24:22 +0530

    commit changes

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  
+  <body>
+      <div class="container">
+          This is
+        <span class="a" id="time"></span>
+      </div>
+      <script>
+         abc = ()=>{
+             console.log("hello");
+         }
+         setTimeout(() => {
+             document.getElementById('time').innerHTML=("hello");
+         }, 2000);
+     </script>
+  </script>
+  </body>
+  
+  </html>`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
